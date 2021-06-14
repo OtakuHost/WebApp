89bc7b46f8456356a15597061cb81044dd62c581
Otaku Host App <68518222+otakuhostapp@users.noreply.github.com>
2021-06-14 16:39:25 -0300

    Update AnimesFontes.js

--- AnimesFontes.js
+++ AnimesFontes.js
@@ -99,7 +99,7 @@ function load_fonts_Animes(){
             TFontes:async function(Fontes){
                 if((Fontes[0]).includes("/0/bg.mp4")){
                     let temp = Fontes[0];
-                    console.log("Url:"+Fontes[0]);
+                    console.log("Url2:"+Fontes[0]);
                     WebApp.UrlRedirect("https://www.anitube.site/?s=One+Piece",Fontes[0],''+function(url){
                         aux.FontsVids[aux.FontSelect][0] = url;
                         aux.FontsVids[aux.FontSelect][1] = false;

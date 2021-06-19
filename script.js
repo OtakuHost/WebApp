dbaae346f488486e5df3c3dced20ca3cd1f16d09
Otaku Host App <68518222+OtakuHost@users.noreply.github.com>
2021-06-19 16:16:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1164,7 +1164,7 @@ function tratamento(txt,biblioteca){
 }
 
 async function torrentStream(Hash){
-	WebApp.StreamTorrent(Hash,''async function(FileList){
+	WebApp.StreamTorrent(Hash,''+async function(FileList){
 		console.log("TorrentPuta: "+FileList);
 	});
 }
